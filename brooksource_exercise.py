a2ff130f442b5d5554feb54870bedfb4f3463bdb
newbsaibot <32990956+newbsaibot@users.noreply.github.com>
2019-05-21 10:44:34 -0400

    Add files via upload

---
+++ brooksource_exercise.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 11 15:34:33 2019
+
+@author: iamgroot
+"""
+
+provider1 = []
+provider2 = []
+provider3 = []
+
+user_input = input('Select a provider :')
+for i in user_input:
+    if i in provider1:
+        p1 = provider1.append(i)
+        print(provider1)
+        
+for x in user_input:
+    if x == provider2:
+        p2 = provider2.append(x)
+        print(provider2)
+        
+for y in user_input:
+    if y == user_input:
+        p3 = provider3.append(y)
+        print(provider3)
+        
+        
+    
+        
+
+
+            
+
+        
+
+
+
+
+
+
+
+
+       
+      
